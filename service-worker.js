cc75949b19c82d23d56925722dd5d52747682017
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-10 13:44:45 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/electrical-monitoring-dashboard-react/precache-manifest.8aa95c82e90a02fbf26c57fd7796ab2d.js"
+  "/electrical-monitoring-dashboard-react/precache-manifest.8d8853164d9efa78ff0b68fefc5f5655.js"
 );
 
 self.addEventListener('message', (event) => {
