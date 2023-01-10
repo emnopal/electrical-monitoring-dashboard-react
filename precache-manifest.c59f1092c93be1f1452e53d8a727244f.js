707ce47a89f951e9a8290714ab4408acbc1c2618
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-10 11:22:03 +0700

    Updates

---
+++ precache-manifest.c59f1092c93be1f1452e53d8a727244f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95d6f56bfe0f0240e267ab716ffe7da",
+    "url": "/electrical-monitoring-dashboard-react/index.html"
+  },
+  {
+    "revision": "32522855481f9ade1848",
+    "url": "/electrical-monitoring-dashboard-react/static/css/2.f0e33c08.chunk.css"
+  },
+  {
+    "revision": "b2be80fac3aab5626840",
+    "url": "/electrical-monitoring-dashboard-react/static/css/main.011e96f4.chunk.css"
+  },
+  {
+    "revision": "32522855481f9ade1848",
+    "url": "/electrical-monitoring-dashboard-react/static/js/2.4a2bf9d8.chunk.js"
+  },
+  {
+    "revision": "dfd0a0c0f3e6c9e64a9162ef9b65ce51",
+    "url": "/electrical-monitoring-dashboard-react/static/js/2.4a2bf9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2be80fac3aab5626840",
+    "url": "/electrical-monitoring-dashboard-react/static/js/main.a02970a0.chunk.js"
+  },
+  {
+    "revision": "f414d87eeab74d3ea939",
+    "url": "/electrical-monitoring-dashboard-react/static/js/runtime-main.d928dce7.js"
+  }
+]);
