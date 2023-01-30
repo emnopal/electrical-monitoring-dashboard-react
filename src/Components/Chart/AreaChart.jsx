e2f9e54cd626185eb0e047ce5c3550729b1adb7e
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-30 14:12:26 +0700

    moved to Component/Chart

---
+++ src/Components/Chart/AreaChart.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Highcharts from "highcharts";
+
+class AreaChart extends Component {
+	componentDidUpdate() {
+		this.createAreaChart();
+	}
+
+	componentDidMount() {
+		this.createAreaChart();
+	}
+
+	createAreaChart() {
+		let formatData = [];
+
+		formatData = this.props.data
+			.map((obj) => {
+				const dateInSeconds = new Date(obj["timestamp"]*1000).getTime();
+				return [dateInSeconds, obj[this.props.title]];
+			})
+			.reverse();
+
+		Highcharts.chart("areaChart", {
+			rangeSelector: {
+				selected: 1,
+			},
+
+			chart: {
+				type: "area",
+			},
+
+			title: {
+				text: this.props.title,
+			},
+
+			series: [
+				{
+					name: "",
+					data: formatData,
+					tooltip: {
+						valueDecimals: 2,
+					},
+				},
+			],
+
+			xAxis: {
+				allowDecimals: false,
+				labels: {
+					formatter: function () {
+						const date = new Date(this.value); // 2009-11-10
+						return date.toLocaleString("id-ID", {
+							year: "numeric",
+							month: "short",
+						});
+					},
+				},
+			},
+
+			yAxis: {
+				title: {
+					text: this.props.title,
+				},
+				labels: {
+					formatter: function () {
+						return this.value;
+					},
+				},
+			},
+
+			tooltip: {
+				formatter: function () {
+					let tooltip = Highcharts.dateFormat(
+						"%A %b %e, %Y",
+						new Date(this.x),
+					);
+                    tooltip += "<br/>" + this.y
+					return tooltip;
+				},
+			},
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<div id="areaChart" />
+			</div>
+		);
+	}
+}
+
+AreaChart.propTypes = {
+	title: PropTypes.string,
+	data: PropTypes.array,
+};
+
+export default AreaChart;
---
+++ src/Components/Chart/StatisticsChart.jsx
@@ -0,0 +1,183 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import StatFuncFromObj from "../../Helper/Utils/statFuncFromObj";
+
+class StatisticsChart extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: props.title,
+			data: props.data,
+			units: props.units,
+			highest: StatFuncFromObj.sortObj(props.data, props.title)[0][props.title],
+			lowest: StatFuncFromObj.sortObj(props.data, props.title, "desc")[0][
+				props.title
+			],
+			timeHighest: StatFuncFromObj.convertLocale(
+				StatFuncFromObj.sortObj(props.data, props.title)[0]["timestamp"],
+			),
+			timeLowest: StatFuncFromObj.convertLocale(
+				StatFuncFromObj.sortObj(props.data, props.title, "desc")[0]["timestamp"],
+			),
+			mean: StatFuncFromObj.findMean(props.data, props.title),
+			median: StatFuncFromObj.findMedian(props.data, props.title),
+			std: StatFuncFromObj.findStd(props.data, props.title),
+			latestData: StatFuncFromObj.sortObj(props.data, "timestamp")[0][props.title],
+			latestTime: StatFuncFromObj.convertLocale(
+				StatFuncFromObj.sortObj(props.data, "timestamp")[0]["timestamp"],
+			),
+			firstData: StatFuncFromObj.sortObj(props.data, "timestamp", "desc")[0][
+				props.title
+			],
+			firstTime: StatFuncFromObj.convertLocale(
+				StatFuncFromObj.sortObj(props.data, "timestamp", "desc")[0]["timestamp"],
+			),
+		};
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (
+			prevProps.title !== this.props.title &&
+			prevProps.data !== this.props.data
+		) {
+			this.setState({
+				title: this.props.title,
+				data: this.props.data,
+				units: this.props.units,
+				highest: StatFuncFromObj.sortObj(this.props.data, this.props.title)[0][
+					this.props.title
+				],
+				lowest: StatFuncFromObj.sortObj(
+					this.props.data,
+					this.props.title,
+					"desc",
+				)[0][this.props.title],
+				timeHighest: StatFuncFromObj.convertLocale(
+					StatFuncFromObj.sortObj(this.props.data, this.props.title)[0][
+						"timestamp"
+					],
+				),
+				timeLowest: StatFuncFromObj.convertLocale(
+					StatFuncFromObj.sortObj(this.props.data, this.props.title, "desc")[0][
+						"timestamp"
+					],
+				),
+				mean: StatFuncFromObj.findMean(this.props.data, this.props.title),
+				median: StatFuncFromObj.findMedian(this.props.data, this.props.title),
+                std: StatFuncFromObj.findStd(this.props.data, this.props.title),
+				latestData: StatFuncFromObj.sortObj(this.props.data, "timestamp")[0][
+					this.props.title
+				],
+				latestTime: StatFuncFromObj.convertLocale(
+					StatFuncFromObj.sortObj(this.props.data, "timestamp")[0]["timestamp"],
+				),
+				firstData: StatFuncFromObj.sortObj(
+					this.props.data,
+					"timestamp",
+					"desc",
+				)[0][this.props.title],
+				firstTime: StatFuncFromObj.convertLocale(
+					StatFuncFromObj.sortObj(this.props.data, "timestamp", "desc")[0][
+						"timestamp"
+					],
+				),
+			});
+		}
+	}
+
+	renderOther() {
+		return (
+			<div>
+				<h6>
+					Mean {this.state.title}: {this.state.mean}{" "}
+					{this.state.units}
+				</h6>
+				<h6>
+					Median {this.state.title}: {this.state.median}{" "}
+					{this.state.units}
+				</h6>
+				<h6>
+					Standard Deviation {this.state.title}: {this.state.std}{" "}
+					{this.state.units}
+				</h6>
+				<br />
+				<h6>
+					Highest {this.state.title}: {this.state.highest}{" "}
+					{this.state.units} ({this.state.timeHighest})
+				</h6>
+				<h6>
+					Lowest {this.state.title}: {this.state.lowest}{" "}
+					{this.state.units} ({this.state.timeLowest})
+				</h6>
+				<br />
+				<h6>
+					Latest Data of {this.state.title}: {this.state.latestData}{" "}
+					{this.state.units} ({this.state.latestTime})
+				</h6>
+				<h6>
+					First Data of {this.state.title}: {this.state.firstData}{" "}
+					{this.state.units} ({this.state.firstTime})
+				</h6>
+			</div>
+		);
+	}
+
+	renderCurrency() {
+		return (
+			<div>
+				<h6>
+					Mean {this.state.title}: {this.state.units}
+					{this.state.mean}
+				</h6>
+				<h6>
+					Median {this.state.title}: {this.state.units}
+					{this.state.median}
+				</h6>
+				<h6>
+					Standard Deviation {this.state.title}: {this.state.units}
+					{this.state.std}
+				</h6>
+				<br />
+				<h6>
+					Highest {this.state.title}: {this.state.units}
+					{this.state.highest} ({this.state.timeHighest})
+				</h6>
+				<h6>
+					Lowest {this.state.title}: {this.state.units}
+					{this.state.lowest} ({this.state.timeLowest})
+				</h6>
+				<br />
+				<h6>
+					Latest Data of {this.state.title}: {this.state.units}
+					{this.state.latestData} ({this.state.latestTime})
+				</h6>
+				<h6>
+					First Data of {this.state.title}: {this.state.units}
+					{this.state.firstData} ({this.state.firstTime})
+				</h6>
+			</div>
+		);
+	}
+
+	render() {
+        let conditionalUnits;
+        if(this.state.title === 'Total Price') {
+            conditionalUnits = this.renderCurrency();
+        } else {
+            conditionalUnits = this.renderOther();
+        }
+		return (
+            <div>
+                {conditionalUnits}
+            </div>
+        );
+	}
+}
+
+StatisticsChart.propTypes = {
+    title: PropTypes.string,
+    data: PropTypes.array,
+    units: PropTypes.string
+}
+
+export default StatisticsChart;
