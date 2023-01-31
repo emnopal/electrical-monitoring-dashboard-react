342dbfc9eb449411f588e6d46db2b1fe01933bc5
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-31 12:17:52 +0700

    add css to index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import './Style/DarkMode.css';
+import './Style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
