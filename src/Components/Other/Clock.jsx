b2cbb79edf5c381a0c3dba36920ef6636e55762b
Muhammad Naufal <mnaufal0999@gmail.com>
2023-01-31 12:19:15 +0700

    add realtime clock

--- src/Components/Other/Clock.jsx
+++ src/Components/Other/Clock.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class Clock extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			time: new Date().toLocaleString(),
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().toLocaleString(),
+        };
+    }
 
-	componentDidMount() {
-		this.intervalID = setInterval(() => this.tick(), 1000);
-	}
+    componentDidMount() {
+        this.intervalID = setInterval(() => this.tick(), 1000);
+    }
 
-	componentWillUnmount() {
-		clearInterval(this.intervalID);
-	}
+    componentWillUnmount() {
+        clearInterval(this.intervalID);
+    }
 
-	tick() {
-		this.setState({
-			time: new Date().toLocaleString(),
-		});
-	}
+    tick() {
+        this.setState({
+            time: new Date().toLocaleString(),
+        });
+    }
 
-	render() {
-		return (
-            <p className="App-clock">
-                Current UTC+7 Time: {this.state.time}
-            </p>
+    render() {
+        return (
+            <span className="App-clock">
+                Current datetime: {this.state.time}
+            </span>
         )
-	}
+    }
 }
 
 export default Clock;
